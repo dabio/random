96584f97221a126314a6bb46ce26b8a5ee565025
Danilo <dabio@users.noreply.github.com>
2015-04-26 21:02:04 +0200

    Allow creating, retrieving and updating of items.

--- msg/src/dabio/RestBundle/Controller/ItemController.php
+++ msg/src/dabio/RestBundle/Controller/ItemController.php
@@ -2,9 +2,13 @@
 
 namespace dabio\RestBundle\Controller;
 
+use dabio\RestBundle\Entity\Item;
 use dabio\RestBundle\Service\EntityFormatter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * Class ItemController
@@ -13,6 +17,11 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class ItemController extends Controller
 {
+    /**
+     * Returns all items in a json array.
+     *
+     * @return JsonResponse
+     */
     public function listAction()
     {
         $items = $this
@@ -24,5 +33,106 @@ class ItemController extends Controller
         return new JsonResponse(
             (new EntityFormatter())->itemsArrayFormat($items)
         );
+    }
+
+    /**
+     * Creates a new item with the given parameters.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse|Response
+     */
+    public function createAction(Request $request)
+    {
+        $item = new Item();
+        $form = $this->getItemForm($item)->setMethod('POST')->getForm();
+
+        if (!$form->handleRequest($request)->isValid()) {
+            // Something went wrong.
+            throw new HttpException(422, 'Unprocessable Entity');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($item);
+        $em->flush();
+
+        // get url of newly created item
+        $itemUrl = $this->generateUrl(
+            'dabio_rest_item_read',
+            ['id' => $item->getId()]
+        );
+
+        return new JsonResponse(
+            (new EntityFormatter())->itemArrayFormat($item),
+            201,
+            ['Location' => $itemUrl]
+        );
+    }
+
+    /**
+     * Gets an item.
+     *
+     * @param Item $item
+     *
+     * @return JsonResponse
+     */
+    public function readAction(Item $item)
+    {
+        return new JsonResponse(
+            (new EntityFormatter())->itemArrayFormat($item)
+        );
+    }
+
+    /**
+     * Updates an item.
+     *
+     * @param Request $request
+     * @param Item    $item
+     *
+     * @return JsonResponse
+     */
+    public function updateAction(Request $request, Item $item)
+    {
+        $form = $this->getItemForm($item)->setMethod('PUT')->getForm();
+
+        if (!$form->submit($request, false)->isValid()) {
+            // Something went wrong.
+            throw new HttpException(422, 'Unprocessable Entity');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($item);
+        $em->flush();
+
+        return new JsonResponse(
+            (new EntityFormatter())->itemArrayFormat($item)
+        );
+    }
+
+    /**
+     * HELPERS
+     *
+     * Find a few helpers below.
+     */
+
+    /**
+     * Form for creating and editing items.
+     *
+     * @param Item $item
+     *
+     * @return \Symfony\Component\Form\FormBuilderInterface
+     */
+    private function getItemForm(Item $item)
+    {
+        // We use the form.factory service here, so we can use a form without
+        // a name.
+        /** @var \Symfony\Component\Form\FormFactory $form */
+        $form = $this->get('form.factory');
+
+        return $form
+            ->createNamedBuilder('', 'form', $item)
+            ->add('name')
+            ->add('description')
+            ->add('price', 'number');
     }
 }
--- msg/src/dabio/RestBundle/Tests/Controller/ItemControllerTest.php
+++ msg/src/dabio/RestBundle/Tests/Controller/ItemControllerTest.php
@@ -80,4 +80,117 @@ class ItemControllerTest extends WebTestCase
             $this->assertNotEmpty($json[$key]->id);
         }
     }
+
+    /**
+     * Tests whether an item can get created through our endpoint.
+     */
+    public function testCreate()
+    {
+        $itemData = [
+            'name' => 'Item 3 Name',
+            'description' => 'Item 3 Description',
+            'price' => 3
+        ];
+
+        $client = static::createClient();
+        $client->request('POST', '/items', $itemData);
+
+        $this->assertEquals(201, $client->getResponse()->getStatusCode());
+
+        $json = json_decode($client->getResponse()->getContent());
+        $this->assertEquals($itemData['name'], $json->name);
+        $this->assertEquals($itemData['description'], $json->description);
+        $this->assertEquals($itemData['price'], $json->price);
+        $this->assertNotEmpty($json->id);
+    }
+
+    /**
+     * Checks if the endpoint returns our item.
+     */
+    public function testRead()
+    {
+        // get an item
+        /** @var \dabio\RestBundle\Entity\Item $item */
+        $item = $this->em->getRepository('dabioRestBundle:Item')->findOneBy([
+            'name' => ItemFixtures::getTestItem1()->getName()
+        ]);
+
+        $client = static::createClient();
+        $client->request('GET', sprintf('/items/%s', $item->getId()));
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $json = json_decode($client->getResponse()->getContent());
+        $this->assertEquals($item->getId(), $json->id);
+        $this->assertEquals($item->getName(), $json->name);
+        $this->assertEquals($item->getDescription(), $json->description);
+        $this->assertEquals($item->getPrice(), $json->price);
+    }
+
+    /**
+     * Checks if our server returns 404 when an item was not found.
+     */
+    public function testRead404()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/items/somethingAbsent');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Checks if updating a name of an item is working.
+     */
+    public function testUpdateName()
+    {
+        // get an item
+        /** @var \dabio\RestBundle\Entity\Item $item */
+        $item = $this->em->getRepository('dabioRestBundle:Item')->findOneBy([
+            'name' => ItemFixtures::getTestItem1()->getName()
+        ]);
+
+        $itemData = [
+            'name' => sprintf('A %s name', uniqid())
+        ];
+
+        $client = static::createClient();
+        $client->request('PUT', sprintf('/items/%s', $item->getId()), $itemData);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $json = json_decode($client->getResponse()->getContent());
+        $this->assertEquals($item->getId(), $json->id);
+        $this->assertEquals($itemData['name'], $json->name);
+        $this->assertEquals($item->getDescription(), $json->description);
+        $this->assertEquals($item->getPrice(), $json->price);
+    }
+
+    /**
+     * Tests updating all attributes of an item.
+     */
+    public function testUpdateItem()
+    {
+        // get an item
+        /** @var \dabio\RestBundle\Entity\Item $item */
+        $item = $this->em->getRepository('dabioRestBundle:Item')->findOneBy([
+            'name' => ItemFixtures::getTestItem1()->getName()
+        ]);
+
+        $itemData = [
+            'name' => sprintf('A %s name', uniqid()),
+            'description' => sprintf('A %s description', uniqid()),
+            'price' => 89000.45,
+        ];
+
+        $client = static::createClient();
+        $client->request('PUT', sprintf('/items/%s', $item->getId()), $itemData);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $json = json_decode($client->getResponse()->getContent());
+        $this->assertEquals($item->getId(), $json->id);
+        $this->assertEquals($itemData['name'], $json->name);
+        $this->assertEquals($itemData['description'], $json->description);
+        $this->assertEquals($itemData['price'], $json->price);
+    }
 }
